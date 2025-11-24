151fcd4b94117b89465e283ee18504f176944dac
guigas-cmd <diasnascimentoguilherme@gmail.com>
2025-11-24 18:48:08 -0300

    static PI, circunference and volume

---
+++ src/Program.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Program {
+    public static final double PI = 3.14159;  // const
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter radius: ");
+        double radius = sc.nextDouble();
+        double c = circunference(radius);
+        double v = volume(radius);
+
+        System.out.printf("CIRCUNFERENCE: %.2f \n ", c);
+        System.out.printf("VOLUME: %.2f \n ", v);
+        System.out.printf("PI value: %.2f \n ", PI);
+
+        sc.close();
+    }
+
+    public static double circunference(double radius) {
+        return 2.0 * PI * radius;
+    }
+    public static double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3.0;
+    }
+}
