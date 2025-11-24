923be3592da710cda2d5545d511189f876a7df01
guigas-cmd <diasnascimentoguilherme@gmail.com>
2025-11-24 19:25:32 -0300

    Calculator added

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,26 @@
+package application;
+import util.Calculator;
 import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Program {
-    public static final double PI = 3.14159;  // const
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        Calculator calc = new Calculator();
+
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
-        double c = circunference(radius);
-        double v = volume(radius);
+        double c = calc.circunference(radius);
+        double v = calc.volume(radius);
 
         System.out.printf("CIRCUNFERENCE: %.2f \n ", c);
         System.out.printf("VOLUME: %.2f \n ", v);
-        System.out.printf("PI value: %.2f \n ", PI);
+        System.out.printf("PI value: %.2f \n ", calc.PI);
 
         sc.close();
     }
 
-    public static double circunference(double radius) {
-        return 2.0 * PI * radius;
-    }
-    public static double volume(double radius) {
-        return 4.0 * PI * radius * radius * radius / 3.0;
-    }
 }
