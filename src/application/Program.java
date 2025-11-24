abe87d6f186b98c8104931a7a13feb2d1eae74a3
guigas-cmd <diasnascimentoguilherme@gmail.com>
2025-11-24 20:33:29 -0300

    static added

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,16 +9,14 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Calculator calc = new Calculator();
-
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
-        double c = calc.circunference(radius);
-        double v = calc.volume(radius);
+        double c = Calculator.circumference(radius);
+        double v = Calculator.volume(radius);
 
         System.out.printf("CIRCUNFERENCE: %.2f \n ", c);
         System.out.printf("VOLUME: %.2f \n ", v);
-        System.out.printf("PI value: %.2f \n ", calc.PI);
+        System.out.printf("PI value: %.2f \n ", Calculator.PI);
 
         sc.close();
     }
--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -1,11 +1,11 @@
 package util;
 
 public class Calculator {
-    public double PI = 3.14159;
-    public double circunference(double radius) {
+    public static final double PI = 3.14159;
+    public static double circumference(double radius) {
         return 2.0 * PI * radius;
     }
-    public double volume(double radius) {
+    public static double volume(double radius) {
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 
